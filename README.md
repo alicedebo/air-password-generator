6ecda7a9622b263f60464d2c6e734b96d5c2129e
Allison Richardel <airichardel@gmail.com>
2021-07-13 14:50:01 -0600

    Final push, ready to deploy

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# air-password-generator
+# Password Generator
+
+## Attempt
+This was my attempt at making a password generator. I have failed in a few parts, but as long as you want all of the criteria, <br>
+it works great! Here is a preview of how it looks:<br>
+![screenshot of password generated](./pg.png)<br>
+Would you like to see how it works?
+[Here's the link!](https://alicedebo.github.io/air-password-generator)
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let generateBtn = document.querySelector("#generate");
-const randomFunc = [getRandomLower, getRandomUpper, getRandomNumber, getRandomSymbol]
+let randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
 
 
 // generator functions
@@ -22,6 +27,7 @@ function getRandomSymbol() {
 
 function getPassword(getRandomUpper, getRandomLower, getRandomSymbol, getRandomNumber, passwordLength){
 let password = ""
+
 for (let i = 0; i < passwordLength; i++) {
   if (Math.random() < 0.2){
     password += getRandomUpper();
@@ -35,39 +41,30 @@ for (let i = 0; i < passwordLength; i++) {
     else if (Math.random() <= 0.6){
       password += getRandomNumber();
     }
+    
   }
+ 
   return password
 }
 
-// console.log(getRandomUpper());
-// console.log(getRandomLower());
-// console.log(getRandomNumber());
-// console.log(getRandomSymbol());
-
-
 function generatePassword() {
-  
   let passwordLength = window.prompt("How many characters would you like your password to be?")
+  let allValues = (getRandomUpper, getRandomLower, getRandomNumber, getRandomSymbol)
 
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Please enter a number between 8 and 128.")
     // generatePassword.push(passwordLength)
     return
   } else {
-    let getRandomUpper = window.confirm("Would you like to use Upper Case Characters?");
-    let getRandomLower = window.confirm("Would you like to use Lower Case Characters?");
-    let getRandomSymbol = window.confirm("Would you like to use Special Characters?");
-    let getRandomNumber = window.confirm("Would you like to use Number Characters?");
-
-    
-    // for (let i = 0; i < passwordLength; i++) {
-    //   passwordLength + randomFunc; 
-    //   generatePassword = randomFunc;
-    // }
+    let getRandomUpper = window.confirm("Would you like to use UPPER Case Characters?");
+    let getRandomLower = window.confirm("Would you like to use lower Case Characters?");
+    let getRandomSymbol = window.confirm("Would you like to use $pecial Characters?");
+    let getRandomNumber = window.confirm("Would you like to use Number# Characters?");
   }
+   
   return getPassword(getRandomUpper, getRandomLower, getRandomSymbol, getRandomNumber, passwordLength)
-}
 
+}
 
 generateBtn.addEventListener("click", writePassword);
 
@@ -78,25 +75,3 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
- 
-  //store that response in a variable/array
-  //comfirm for use lower case letters
-  //comfirm for upper case letters
-  //confirm for special characters
-  //confirm for numbers
-  //create an array of lower case, upper case, special, and numbers
-  // randomly generate the included characters. math.random, math.floor
-  // mix them up into random string
-
-
-
-
-// Click button to generate password.
-// When button is clicked, series of prompts for password criteria.
-// when prompted, select criteria to include in the password
-// length of pass must be between 8-128
-// prompt character types, confirm to include lowercase, uppercase, numeric, and/or special characters.
-// when answer prompts, input should be validated and at least one character type should be selected.
-// when all prompts answered, password is generated that matches selected criteria.
-// when pass is generated, pass is displayed in an alert or written to the page.
